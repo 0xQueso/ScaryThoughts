ac627c199953b05add1f8567ef80278fb70904f7
0xqueso <0xqueso@gmail.com>
2021-10-26 03:54:50 +0800

    fix frontend

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import React, {useEffect, useState} from "react";
 import {ethers} from "ethers";
 import {abi} from "./utils/abi.json";
 
-// Constants
+
 const TWITTER_HANDLE = '@queso';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-const OPENSEA_LINK = '';
-const TOTAL_MINT_COUNT = 50;
+const CONTRACT_ADDRESS = "0x322B58984ee05f982d95B1C2FeF64D3C85692Cf5"
 
 const App = () => {
-  // Render Methods
+
   const [currentAccount, setCurrentAccount] = useState("");
   const [minting, setMinting] = useState(false);
+  const [svgContent, setSvgContent] = useState();
+  const [openseaLink, setOpenseaLink] = useState();
 
   const checkIfWalletIsConnected = async () => {
     const { ethereum } = window;
@@ -63,8 +64,6 @@ const App = () => {
   }
 
   const askContractToMintNft = async () => {
-    const CONTRACT_ADDRESS = "0xB93c94f4920f065bCC64bC88f7f180be2072Db5e"
-
     try {
       const { ethereum } = window;
       if (ethereum) {
@@ -73,8 +72,8 @@ const App = () => {
         const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
 
         console.log(parseInt(await connectedContract.getMintedNft()), 'here');
-
-        let nftTxn = await connectedContract.makeAnEpicNFT();
+        // console.log('bal',provider.getBalance(CONTRACT_ADDRESS).then(b => console.log(parseInt(b))));
+        let nftTxn = await connectedContract.makeAnEpicNFT({value: 0.005 * 10**18});
         setMinting(true);
         nftTxn.wait();
         console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
@@ -82,10 +81,14 @@ const App = () => {
         let mintedSupply = parseInt(await connectedContract.getMintedNft());
         let totalSupply = parseInt(await connectedContract.getTotalSupply());
 
-        connectedContract.on("JustMinted", (from, tokenId) => {
+        connectedContract.on("JustMinted", async (from, tokenId) => {
           setMinting(false);
-          console.log(from, tokenId.toNumber())
-          alert(`supply: ${mintedSupply} / ${totalSupply} <br/> link to minted nft <https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}>`)
+          let nftTxn = await connectedContract.tokenURI(tokenId.toNumber());
+          let obj = (atob(nftTxn.replace("data:application/json;base64,","")));
+          setSvgContent(JSON.parse(obj).image);
+          setOpenseaLink(`https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`);
+          console.log(from, tokenId.toNumber());
+          alert(`current nft supply: ${mintedSupply + 1} / ${totalSupply}`)
         })
       } else {
         console.log("no eth")
@@ -94,7 +97,26 @@ const App = () => {
       console.log(e)
     }
   }
-  const renderNotConnectedContainer = () => (
+
+  const withdraw = async () => {
+
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
+
+        let nftTxn = await connectedContract.withdraw();
+        nftTxn.wait();
+      } else {
+        console.log("no eth")
+      }
+    } catch (e) {
+      console.log(e)
+    }
+  }
+  const renderNotConnectedContainer = (msg) => (
       <>
         {currentAccount === "" ? (
           <button onClick={connectWallet} className="cta-button connect-wallet-button">
@@ -102,7 +124,7 @@ const App = () => {
           </button>
       ) : (
           <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
-            {minting ? "Minting..." : "Mint NFT" }
+            {minting ? "Minting..." : msg }
           </button>
       )}
       </>
@@ -115,18 +137,42 @@ const App = () => {
         <div className="header-container">
           <p className="header gradient-text">My NFT Collection</p>
           <p className="sub-text">
-            Each unique. Each beautiful. Discover your NFT today.
+            { svgContent ? "Succesfully minted" : "Each unique. Each beautiful. Discover your NFT today."}
           </p>
-          {renderNotConnectedContainer()}
+          { svgContent &&
+          <a
+              className="footer-text"
+              href={openseaLink}
+              target="_blank"
+              rel="noreferrer"
+          >Opensea link here</a>
+          }
+          <div>
+            {svgContent &&  <img width={350} src={svgContent}/>}
+
+          </div>
+          <div>
+            {svgContent ? renderNotConnectedContainer("Mint Again") : renderNotConnectedContainer("Mint NFT")}
+
+          </div>
+
+
         </div>
         <div className="footer-container">
+          { false &&
+            <button onClick={withdraw} className="cta-button connect-wallet-button">
+              withdraw
+            </button>
+          }
+
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
             className="footer-text"
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`built by ${TWITTER_HANDLE}`}</a>
+
         </div>
       </div>
     </div>
